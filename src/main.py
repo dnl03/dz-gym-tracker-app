49c75c1438b586b032f731d47e395192773978e0
unknown <daniel.zeman08@gmail.com>
2025-08-19 09:30:40 +0200

    imlement enpoint to create one max rep for logged in users

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from db.exercise_repository import seed_exercises
 from api.auth import router as auth_router
 from api.profile import router as profile_router
 from api.exercises import router as exercises_router
+from api.max_rep import router as max_rep_router
 from sqlmodel import Session
 
 
@@ -19,3 +20,4 @@ def on_startup():
 app.include_router(auth_router)
 app.include_router(profile_router)
 app.include_router(exercises_router)
+app.include_router(max_rep_router)
